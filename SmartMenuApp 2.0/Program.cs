7bba6806e5e587e8b061a386f5803e1d0e44cadc
Murad Ali Ahmad Hassan <mura0273@edu.eal.dk>
2018-10-08 13:06:07 +0200

    Add project files.

---
+++ SmartMenuApp 2.0/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartMenuLibrary;
+
+namespace SmartMenuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        private void Run()
+        {
+
+
+            SmartMenu menu = new SmartMenu();
+            Console.WriteLine("Vælg sprog / Choose language ");
+            Console.WriteLine("For Dansk: Tryk 1 / For English press 2");
+            string input = Console.ReadLine();
+            int.TryParse(input, out int r);
+            menu.LoadMenu(menu.ChooseLanguage(r));
+            menu.Activate();
+        }
+
+    }
+}
